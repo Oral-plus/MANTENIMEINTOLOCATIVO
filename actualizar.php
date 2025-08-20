ee68d5ed5c2c207225eb3d654d79fb827ab06795
unknown <ADMINISTRADOR@SKY.LOCAL>
2025-08-20 16:27:12 -0500

    first commit

---
+++ actualizar.php
@@ -0,0 +1,48 @@
+<?php 
+$serverName ="HERCULES";
+$connectionInfo = array( "Database"=>"calidad", "UID"=>"sa", "PWD"=>"Sky2022*!");
+$conexion =sqlsrv_connect ($serverName, $connectionInfo);
+
+$id=$_GET['id'];
+
+
+$sql="SELECT * FROM MANTENIMIENTOLOCATIVO WHERE ORDEN='$id'";
+$query=sqlsrv_query($conexion,$sql);
+
+$row=sqlsrv_fetch_array($query);
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title></title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link href="css/style.css" rel="stylesheet">
+        <title>Actualizar</title>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+       
+    </head>
+    <body>
+                <div class="container mt-5">
+                    <form action="update.php" method="POST">
+                    
+                                <input type="hidden" name="ORDEN" value="<?php echo $row['ORDEN']  ?>">
+                                
+                               
+                                <label for="TIEMPO_EMPLEADO" class="formulario__label">TIEMPO_EMPLEADO INICIO:</label>
+                                <input type="time" class="form-control mb-3" name="TIEMPO_EMPLEADO" placeholder="TIEMPO_EMPLEADO" value="<?php echo $row['TIEMPO_EMPLEADO']  ?>">
+                                <label for="TIEMPO_EMPLEADO_FIN" class="formulario__label">TIEMPO_EMPLEADO FIN:</label>
+                                <input type="time" class="form-control mb-3" name="TIEMPO_EMPLEADO_FIN" placeholder="TIEMPO_EMPLEADO_FIN" value="<?php echo $row['TIEMPO_EMPLEADO_FIN']  ?>">
+                           <label for="TRABAJO_EJECUTADO" class="formulario__label">FECHA TRABAJO_EJECUTADO:</label>
+                                <input type="date" class="form-control mb-3" name="TRABAJO_EJECUTADO" placeholder="TRABAJO_EJECUTADO" value="<?php echo $row['TRABAJO_EJECUTADO']  ?>">-->
+                                <label for="OBSERVACION" class="formulario__label">OBSERVACION:</label>
+                                <input type="text" class="form-control mb-3" name="OBSERVACION" placeholder="OBSERVACION" value="<?php echo $row['OBSERVACION']  ?>"> 
+                                <label for="OBSERVACION" class="formulario__label">SI SE SOLICITO COMPRA DE UN INSUMO INTRODUCIR EL VALOR::</label>
+                                <input type="number" class="form-control mb-3" name="VALOR_REPUESTO" placeholder="VALOR_REPUESTO" value="<?php echo $row['VALOR_REPUESTO']  ?>">
+                                <input type="submit" class="btn btn-primary btn-block" value="Actualizar">
+                    </form>
+                    
+                </div>
+    </body>
+</html>
---
+++ index.php
@@ -0,0 +1,264 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="estilo.css">
+    <title>LOCATIVO</title>
+    <link rel="icon" type="image/x-icon" href="img/logobusqueda.jpg">
+    <link href="https://fonts.googleapis.com/css2?family=Dancing+Script&display=swap" rel="stylesheet">
+   
+</head>
+
+<body>
+    <form method="post" action="index.php">
+        <div class="titulo">
+            <h2> SKY S.A.S<br> MANTENIMIENTO LOCATIVO</h2>
+        </div>
+
+
+        <DIV>
+        
+
+            <label for="EQUIPO" class="formulario__label">EQUIPO O AREA:</label>
+            <select type="text" name="EQUIPO" class="formulario__input" id="EQUIPO"  required>
+                <option value="">SELECCIONA</option>
+                <option value="VENTILADORES INDUSTRIALES">VENTILADORES INDUSTRIALES</option>
+                <option value="DESIONIZADOR">DESIONIZADOR</option>
+                <option value="LABORATORIO FQ">LABORATORIO FQ</option>desionizador
+<option value="ACOPIO DE RESIDUOS PELIGROSOS">ACOPIO DE RESIDUOS PELIGROSOS</option>
+<option value="ACOPIO DE RESIDUOS RECICLABLES">ACOPIO DE RESIDUOS RECICLABLES</option>
+<option value="ARCHIVO RRHH">ARCHIVO RRHH</option>
+<option value="AREA DE ACONDICIONAMIENTO">AREA DE ACONDICIONAMIENTO</option>
+<option value="AREA DE CORTADORA DE CERA">AREA DE CORTADORA DE CERA</option>
+<option value="AREA DE COSMETICOS">AREA DE COSMETICOS</option>
+<option value="AREA DE DEVANADO">AREA DE DEVANADO</option>
+<option value="AREA DE EMPAQUE">AREA DE EMPAQUE</option>
+<option value="AREA DE ENSAMBLE">AREA DE ENSAMBLE</option>
+<option value="AREA DE INYECCION">AREA DE INYECCION</option>
+<option value="AREA DE MARCACION">AREA DE MARCACION</option>
+<option value="AREA DE PREPARACION">AREA DE PREPARACION</option>
+<option value="AREA DE PULIDO">AREA DE PULIDO</option>
+<option value="AREA DE REVELADO">AREA DE REVELADO</option>
+<option value="AREA DE SELLADO TERCER PISO">AREA DE SELLADO TERCER PISO</option>
+<option value="AREA DE SISTEMAS">AREA DE SISTEMAS</option>
+<option value="AREA DE TROQUELADO PRIMER PISO">AREA DE TROQUELADO PRIMER PISO</option>
+<option value="BAÑO MIXTO">BAÑO MIXTO</option>
+<option value="BAÑOS DAMAS PRIMER PISO">BAÑOS DAMAS PRIMER PISO</option>
+<option value="BAÑOS DAMAS SEGUNDO PISO">BAÑOS DAMAS SEGUNDO PISO</option>
+<option value="BAÑOS DAMAS TERCE PISO">BAÑOS DAMAS TERCE PISO</option>
+<option value="BAÑOS HOMBRES PRIMER PISO">BAÑOS HOMBRES PRIMER PISO</option>
+<option value="BAÑOS HOMBRES SEGUNDO PISO">BAÑOS HOMBRES SEGUNDO PISO</option>
+<option value="BAÑOS HOMBRES TERCER PISO">BAÑOS HOMBRES TERCER PISO</option>
+<option value="BODEGA NUEVA">BODEGA NUEVA</option>
+<option value="BODEGA QUINTO PISO">BODEGA QUINTO PISO</option>
+<option value="BODEGA SEXTO PISO">BODEGA SEXTO PISO</option>
+<option value="COCINETA">COCINETA</option>
+<option value="COCINETA BODEGA NUEVA">COCINETA BODEGA NUEVA</option>
+<option value="CUARENTENA">CUARENTENA</option>
+<option value="CUARTO DE LAVADO">CUARTO DE LAVADO</option>
+<option value="CUARTO DE MERCADEO">CUARTO DE MERCADEO</option>
+<option value="CUARTO ELECTRICO PRIMER PISO">CUARTO ELECTRICO PRIMER PISO</option>
+<option value="DESPACHOS">DESPACHOS</option>
+<option value="EMPRESA">EMPRESA</option>
+<option value="EXCLUSA ENTRADA">EXCLUSA ENTRADA</option>
+<option value="EXCLUSA PARTE TRASERA">EXCLUSA PARTE TRASERA</option>
+<option value="LABORATORIO CUARTO PISO">LABORATORIO CUARTO PISO</option>
+<option value="LABORATORIO DE CEPILLOS">LABORATORIO DE CEPILLOS</option>
+<option value="LABORATORIO DE QUINTO PISO">LABORATORIO DE QUINTO PISO</option>
+<option value="LABORATORIO MB">LABORATORIO MB</option>
+<option value="LAVADO DE UNIFORMES">LAVADO DE UNIFORMES</option>
+<option value="MATERIAS PRIMAS PRIMER PISO">MATERIAS PRIMAS PRIMER PISO</option>
+<option value="MATERIAS PRIMAS TERCER PISO">MATERIAS PRIMAS TERCER PISO</option>
+<option value="MUESTRAS DE RETENCION">MUESTRAS DE RETENCION</option>
+<option value="OFICINAS BODEGA NUEVA">OFICINAS BODEGA NUEVA</option>
+<option value="OFICINA DE CALIDAD">OFICINA DE CALIDAD</option>
+<option value="OFICINAS CUARTO PISO">OFICINAS CUARTO PISO</option>
+<option value="PORTERIA">PORTERIA</option>
+<option value="POCETA CUARTO PISO">POCETA CUARTO PISO</option>
+<option value="POCETA PRIMER PISO">POCETA PRIMER PISO</option>
+<option value="POCETA TERCER PISO">POCETA TERCER PISO</option>
+<option value="PRIMEROS AUXILIOS">PRIMEROS AUXILIOS</option>
+<option value="SELLADO">SELLADO</option>
+<option value="SALA DE JUNTAS BODEGA NUEVA">SALA DE JUNTAS BODEGA NUEVA</option>
+<option value="VESTIERES DAMA">VESTIERES DAMA</option>
+<option value="VESTIERES HOMBRES">VESTIERES HOMBRES</option>
+<option value="PRENSADORA">PRENSADORA</option>
+<option value="AREA DE PRENSADORA">AREA DE PRENSADORA</option>
+<option value="SKY GENERAL">SKY GENERAL</option>
+<option value="AREA DE ENGOMADORA">AREA DE ENGOMADORA</option>
+ <option value="PEGADORA DE CAJAS ">PEGADORA DE CAJAS </option>
+ <option value="AREA DE COLAPSIBLE">AREA DE COLAPSIBLE</option>
+ <option value="BODEGA TRINIDAD">BODEGA TRINIDAD</option>
+ <option value="BODEGA PILSEN">BODEGA PILSEN</option>
+
+
+</select>
+
+            <label for="TIPO_DE_MANTENIMIENTO" class="formulario__label">TIPO DE MANTENIMIENTO:</label>
+            <select type="text" name="TIPO_DE_MANTENIMIENTO" class="formulario__input" id="TIPO_DE_MANTENIMIENTO" required >
+
+<option value="">SELECCIONA</option>
+<option value="PREVENTIVO">PREVENTIVO</option>
+<option value="CORRECTIVO">CORRECTIVO</option>
+<option value="COMPRAS">COMPRAS</option>
+<option value="OTROS">OTROS</option>
+
+
+</select>
+            <label for="FECHA" class="formulario__label">FECHA:</label>
+            <input type="date" id="FECHA" value="" name="FECHA">
+
+            <label for="SERVICIO" class="formulario__label">DESCRIPCION DEL SERVICIO SOLICITADO:</label>
+            <input type="text" id="SERVICIO" value="" name="SERVICIO">
+
+            <label for="TIEMPO_EMPLEADO" class="formulario__label">TIEMPO EMPLEADO EN EL MMTTO INICIO:</label>
+            <input type="time" id="TIEMPO_EMPLEADO" value="" name="TIEMPO_EMPLEADO" style="background-color:#659AA6; ">
+
+            <label for="TIEMPO_EMPLEADO_FIN" class="formulario__label">TIEMPO EMPLEADO EN EL MMTTO FIN:</label>
+            <input type="time" id="TIEMPO_EMPLEADO_FIN" value="" name="TIEMPO_EMPLEADO_FIN" style="background-color:
+   ">
+
+
+            <label for="RESPONSABLE" class="formulario__label">RESPONSABLE:</label>
+            <select type="text" name="RESPONSABLE" class="formulario__input" id="RESPONSABLE" >
+
+        <option value="">SELECCIONA</option>
+        <option value="LEONARDO RIOS">LEONARDO RIOS</option>
+        <option value="LUIS MEJIA">LUIS MEJIA</option>
+         <option value="CONTRATISTA">CONTRATISTA</option>
+         <option value="NOSCAR GARCIA">NOSCAR GARCIA</option>
+
+        
+      
+    </select>
+
+
+            <label for="FECHA_PROGRAMACION" class="formulario__label">FECHA PROGRAMACION:</label>
+            <input type="date" id="FECHA_PROGRAMACION" value="" name="FECHA_PROGRAMACION">
+
+            <label for="FECHA_DE_ENTREGA" class="formulario__label">FECHA DE ENTREGA:</label>
+            <input type="date" id="FECHA_DE_ENTREGA" value="" name="FECHA_DE_ENTREGA">
+
+         <label for="TRABAJO_EJECUTADO" class="formulario__label">FECHA TRABAJO EJECUTADO:</label>
+            <input type="date" id="TRABAJO_EJECUTADO" value="" name="TRABAJO_EJECUTADO" style="background-color:#659AA6;">
+ 
+            <label for="OBSERVACION" class="formulario__label">OBSERVACION :</label>
+            <input type="text" id="OBSERVACION" value="" name="OBSERVACION" style="background-color:#659AA6;">
+
+            <label for="VALOR_REPUESTO" class="formulario__label">SI SE SOLICITO COMPRA DE UN INSUMO INTRODUCIR EL VALOR:</label>
+            <input type="number" id="VALOR_REPUESTO" value="" name="VALOR_REPUESTO" >
+ 
+            <center>
+      <input type="submit" value="Enviar" class="btn btn-success" name="btn1" >
+    <a href="index2.php">  <input type="button" value="Consultar" class="btn btn-info" ></a>
+    </center>
+        </DIV>
+
+</form>
+
+    <?php
+
+if(isset($_POST['btn1']))
+{
+    $serverName ="HERCULES";
+    $connectionInfo = array( "Database"=>"calidad", "UID"=>"sa", "PWD"=>"Sky2022*!");
+    $conexion =sqlsrv_connect ($serverName, $connectionInfo);
+
+  $ORDEN = $_POST['ORDEN'];
+  $EQUIPO = $_POST['EQUIPO'];
+ 
+  $TIPO_DE_MANTENIMIENTO = $_POST['TIPO_DE_MANTENIMIENTO'];
+  $FECHA = $_POST['FECHA'];
+  $SERVICIO = $_POST['SERVICIO'];
+  $TIEMPO_EMPLEADO = $_POST['TIEMPO_EMPLEADO'];  
+ 
+  $RESPONSABLE = $_POST['RESPONSABLE'];
+  $FECHA_PROGRAMACION = $_POST['FECHA_PROGRAMACION'];
+  $FECHA_DE_ENTREGA = $_POST['FECHA_DE_ENTREGA'];
+  $TRABAJO_EJECUTADO = $_POST['TRABAJO_EJECUTADO'];  
+  $TIEMPO_EMPLEADO_FIN = $_POST['TIEMPO_EMPLEADO_FIN']; 
+  $OBSERVACION = $_POST['OBSERVACION']; 
+  $VALOR_REPUESTO = $_POST['VALOR_REPUESTO']; 
+
+  sqlsrv_query($conexion, "INSERT INTO MANTENIMIENTOLOCATIVO (EQUIPO,TIPO_DE_MANTENIMIENTO,FECHA,SERVICIO,TIEMPO_EMPLEADO,RESPONSABLE,FECHA_PROGRAMACION,FECHA_DE_ENTREGA,TRABAJO_EJECUTADO,TIEMPO_EMPLEADO_FIN,OBSERVACION,VALOR_REPUESTO)
+   values ('$EQUIPO','$TIPO_DE_MANTENIMIENTO','$FECHA','$SERVICIO','$TIEMPO_EMPLEADO','$RESPONSABLE','$FECHA_PROGRAMACION','$FECHA_DE_ENTREGA','$TRABAJO_EJECUTADO','$TIEMPO_EMPLEADO_FIN','$OBSERVACION','$VALOR_REPUESTO')");
+  //La variable $Conexion viene del archivo "Abrir_Conexion", la cual nos conectara a la base de datos
+  //y de paso hacemos el registro de datos.
+  
+  include("cerrar_conexion.php");
+
+  echo "<SCRIPT >
+alert('Se insertaron Correctamente');
+document.location=('index.php');
+</SCRIPT>";
+}
+
+if(isset($_POST['btn2']))
+{
+    $serverName ="HERCULES";
+    $connectionInfo = array( "Database"=>"calidad", "UID"=>"sa", "PWD"=>"Sky2022*!");
+    $conexion =sqlsrv_connect ($serverName, $connectionInfo);
+
+
+    $ORDEN = $_POST['ORDEN'];
+    if($ORDEN=="") //VERIFICO QUE AGREGEN UN DOCUMENTO OBLIGATORIAMENTE.
+      {echo "Digita un documento por favor. (Ej: 123)";}
+    else
+    {  
+      $resultados = sqlsrv_query($conexion,"SELECT * FROM MANTENIMIENTO WHERE ORDEN = $ORDEN");
+      ?>
+      <div class="col-md-8">
+                            <table class="table" >
+                                <thead class="table-success table-striped" >
+                                    <tr>
+                                        <th>ORDEN</th>
+                                        <th>EQUIPO</th>
+                                        <th>TIPO_DE_MANTENIMIENTO</th>
+                                        <th>FECHA</th>
+                                        <th>SERVICIO</th>
+                                        <th>TIEMPO_EMPLEADO</th>
+                                        <th>RESPONSABLE</th>
+                                        <th>FECHA_PROGRAMACION</th>
+                                        <th>FECHA_DE_ENTREGA</th>
+                               <th>TRABAJO_EJECUTADO</th>
+                                     
+                                        <th></th>
+                                        <th></th>
+                                    </tr>
+                                </thead>
+
+                                <tbody>
+                                        <?php
+                                            while($row=sqlsrv_fetch_array($resultados)){
+                                        ?>
+                                            <tr>
+                                                <th><?php  echo $row['ORDEN']?></th>
+                                                <th><?php  echo $row['EQUIPO']?></th>
+                                                <th><?php  echo $row['TIPO_DE_MANTENIMIENTO']?></th>
+                                                <th><?php  echo $row['FECHA']?></th>
+                                                <th><?php  echo $row['SERVICIO']?></th>
+                                                <th><?php  echo $row['TIEMPO_EMPLEADO']?></th>
+                                                <th><?php  echo $row['RESPONSABLE']?></th>
+                                                <th><?php  echo $row['FECHA_PROGRAMACION']?></th>
+                                                <th><?php  echo $row['FECHA_DE_ENTREGA']?></th>
+                                        <th><?php  echo $row['TRABAJO_EJECUTADO']?></th> 
+                                           
+                                                <th><a href="actualizar.php?id=<?php echo $row['ORDEN'] ?>" class="btn btn-info">Editar</a></th>
+                                                <th><a href="delete.php?id=<?php echo $row['ORDEN'] ?>" class="btn btn-danger">Eliminar</a></th>                                        
+                                            </tr>
+                                        <?php 
+                                            }
+                                        
+    }
+
+  include("cerrar_conexion.php");
+}
+?>
+ 
+
+</body>
+
+</html>
